2ab6364814c709d4f406a51302b4bff20c93a01f
martinswilliam <williamgmca@gmail.com>
2024-02-16 05:38:36 -0300

    add photo profile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,17 @@ export default function Home() {
         William Martins -&nbsp;
           <code className="font-mono font-bold"> Engenheiro de Software</code>
         </p>
+        <p className="flex justify-center">
+
+           <Image
+           className=" flex m-2 place-items-center justify-center rounded-full lg:p-0"   
+           src="/willphoto.svg"
+           alt="GitHub Image"
+           width={130}
+           height={130}
+           priority
+         />
+        </p>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
             className="flex place-items-center gap-2 p-2 lg:pointer-events-auto lg:p-0 "
@@ -77,7 +88,7 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
+      <div className="relative p-8 flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
         
         <h1 className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] priority  font-extrabold text-6xl "
        >will._dev</h1>
